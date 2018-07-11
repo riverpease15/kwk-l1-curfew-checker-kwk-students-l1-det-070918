4fbfe8c87587204a4b913062b8f68fed135d51a5
River Pease <riverpease15@gmail.com>
2018-07-11 15:35:51 +0000

    automatically backed up by learn

---
+++ checker.rb
@@ -0,0 +1,14 @@
+def deluxe_curfew_checker(time)
+  curfew = 11
+  how_much_time = curfew-time
+  if how_much_time == 1
+    puts "You've got one hour left!"
+  elsif how_much_time < 1
+    puts "You're out of time, apparate now!"
+  elsif how_much_time == 2
+    puts "Two hours until curfew!"
+  else
+    puts "You've got hours until curfew, check back later."
+end
+
+deluxe_curfew_checker(11)
